0ab13a48dd18a18ab4094b940900d1de1617ece6
Sined <den-gusev-2005@mail.ru>
2024-04-20 12:34:04 +0300

    Коммит по практике

---
+++ UnitTestOne/Test/UnitTest1.cs
@@ -0,0 +1,83 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestOne;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddEntrySQLRequest1()
+        {
+            string table = "TestTable";
+            string id = "03J";
+            string name = "Buster";
+            string work = "Mannequin";
+            string expected = "INSERT INTO TestTable(ID, Name, Work) VALUES (03J, Buster, Mannequin)";
+            string rvalue = UnitTestOne.TestWorkFromDB.AddEntrySQLRequest(table, id, name, work);
+            Assert.AreEqual(expected, rvalue);
+        }
+
+        [TestMethod]
+        public void TestAddEntrySQLRequest2()
+        {
+            string table = "Void";
+            string id = " ";
+            string name = "Striker";
+            string work = " ";
+            string expected = "INSERT INTO Void(ID, Name, Work) VALUES ( , Striker,  )";
+            string rvalue = UnitTestOne.TestWorkFromDB.AddEntrySQLRequest(table, id, name, work);
+            Assert.AreEqual(expected, rvalue);
+        }
+
+        [TestMethod]
+        public void TestLogInToTheSystem1()
+        {
+            string login = "Kramer03";
+            string password = "20he7ame8as19tarted";
+            bool expected = true;
+            bool rvalue = UnitTestOne.TestWorkFromDB.LogInToTheSystem(login, password);
+            Assert.AreEqual(expected, rvalue);
+        }
+
+        [TestMethod]
+        public void TestLogInToTheSystem2()
+        {
+            string login = "03Kramer";
+            string password = "20he7ame8as19tarted";
+            bool expected = false;
+            bool rvalue = UnitTestOne.TestWorkFromDB.LogInToTheSystem(login, password);
+            Assert.AreEqual(expected, rvalue);
+        }
+
+        [TestMethod]
+        public void TestLogInToTheSystem3()
+        {
+            string login = " Kramer03";
+            string password = "20he";
+            bool expected = false;
+            bool rvalue = UnitTestOne.TestWorkFromDB.LogInToTheSystem(login, password);
+            Assert.AreEqual(expected, rvalue);
+        }
+
+        [TestMethod]
+        public void TestLogInToTheSystem4()
+        {
+            string login = "Kramer_03";
+            string password = "20he";
+            bool expected = false;
+            bool rvalue = UnitTestOne.TestWorkFromDB.LogInToTheSystem(login, password);
+            Assert.AreEqual(expected, rvalue);
+        }
+
+        [TestMethod]
+        public void TestLogInToTheSystem5()
+        {
+            string login = "Kramer03";
+            string password = "20 he";
+            bool expected = false;
+            bool rvalue = UnitTestOne.TestWorkFromDB.LogInToTheSystem(login, password);
+            Assert.AreEqual(expected, rvalue);
+        }
+    }
+}
---
+++ UnitTestOne/UnitTestOne/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+
+namespace UnitTestOne
+{
+    public static class TestWorkFromDB
+    {
+        public static string AddEntrySQLRequest(string table, string id, string name, string work)
+        {
+            if (table == null || id == null || name == null || work == null) 
+            {
+                Console.WriteLine("Error: an empty element!");
+                return null;
+            }
+            else
+            {
+                string SqlRequest = $"INSERT INTO {table}(ID, Name, Work) VALUES ({id}, {name}, {work})";
+                return SqlRequest;
+            }
+        }
+        public static bool LogInToTheSystem(string login, string password)
+        {
+            if (Char.IsDigit(login[0]) || login.StartsWith(" ") || login.Contains("_"))
+            {
+                return false;
+            }
+            if (password.Contains(" "))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    
+}
+
